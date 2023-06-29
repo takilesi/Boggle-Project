c60ce36f68660c4bbac72e9d7b5e4b01dcea6eed
Doug <124453486+takilesi@users.noreply.github.com>
2023-06-29 15:38:49 -0400

    850 word dictionary spell check functioning (additionally with 3 letter words list)

--- tapp.js
+++ tapp.js
@@ -41,17 +41,40 @@
 
 
  
+// this code successfully uses the Type function from above in node environment 
 
-let testVar = "Word 1:      4 pts"; 
-let test2 =0; 
-if (testVar.includes("-")===true){
-    let newTestVar = testVar.split("-"); 
-    let desired = newTestVar[1]; 
-    let test = desired.match(/(\d+)/); 
-    test2 = test[0]; 
-} else {
-  test2 = 0; 
-}
+// let testVar = "Word 1:      4 pts"; 
+// let test2 =0; 
+// if (testVar.includes("-")===true){
+//     let newTestVar = testVar.split("-"); 
+//     let desired = newTestVar[1]; 
+//     let test = desired.match(/(\d+)/); 
+//     test2 = test[0]; 
+// } else {
+//   test2 = 0; 
+// }
 
 
-console.log(test2);
+// console.log(test2);
+
+
+
+const words1 = "come, get, give, go, keep, let, make, put"; 
+
+let wordArray = words1.split(", "); 
+
+console.log(wordArray); 
+
+let tempWord = "fart"; 
+
+// for (i=0; i<wordArray.length; i++) {
+//   // console.log(wordArray[i]); 
+//   if (wordArray[i]==verifyWord){
+//     console.log("true")
+//   } else { 
+//     console.log("false"); 
+//   }
+// }
+
+let check = wordArray.includes(tempWord); 
+console.log(check); 
